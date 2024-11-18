34c5c05985c11b54d1b823edbc79e5ee7600257e
bilgejava6 <bilgejava6@gmail.com>
2024-11-18 14:01:36 +0300

    logo eklendi.

--- src/component/atoms/LeftButtons.tsx
+++ src/component/atoms/LeftButtons.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
-
-function LeftButtons() {
+interface ILeftButtonsProps{
+    iconType: 'Home' | 'Search' | 'Notifications' | 'Message' | 'Profile',
+    name: string,
+    path: string // hangi url adresine (syfaya) yönlenecek
+}
+function LeftButtons(props: ILeftButtonsProps) {
+    const {iconType,name,path} = props;
+    const icons = ()=>{
+    switch(iconType){
+            case 'Home': return <i className="fa-solid fa-house" style={{color: '#1da1f2'}}></i>
+            default: return <></>;
+        }
+    }
   return (
     <>
         <div className="col-2">
-            <i className="fa-solid fa-house" style={{color: '#1da1f2'}}></i>
+            {
+                icons()
+            }
         </div>
         <div className="col-8">
-            <label className='form-label'>Home</label>
+            <label className='form-label'>{name}</label>
         </div>
     </>
   )
 }
-
 export default LeftButtons
--- src/component/molecules/LeftMenuComponent.tsx
+++ src/component/molecules/LeftMenuComponent.tsx
@@ -6,16 +6,16 @@ function LeftMenuComponent() {
     <div className="row justify-content-center">
         <div className="col-5">
             <div className="row mb-2">
-                <LeftButtons />
+                <LeftButtons iconType='Home' name='Anasayfa' path='/home' />
             </div>
             <div className="row mb-2">
-                <LeftButtons />
+                <LeftButtons iconType='Search' name='Keşfet' path='/home'  />
             </div>
             <div className="row mb-2">
-                <LeftButtons />
+                <LeftButtons iconType='Notifications' name='Bildirimler' path='/home'  />
             </div>
             <div className="row mb-2">
-                <LeftButtons />
+                <LeftButtons iconType='Profile' name='Profil' path='/home'  />
             </div>
                     
         </div>
