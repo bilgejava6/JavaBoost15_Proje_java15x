4fe83b36cb6202da48a0847021a3bc59351b8bd7
bilgejava6 <bilgejava6@gmail.com>
2024-11-18 14:43:54 +0300

    leftbutton tamamlandı.

--- src/component/atoms/LeftButtons.tsx
+++ src/component/atoms/LeftButtons.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './LeftButtons.css'
+import { useNavigate } from 'react-router-dom';
 interface ILeftButtonsProps{
     iconType: 'Home' | 'Search' | 'Notifications' | 'Message'
      | 'Profile' | 'Lists' | 'Locations' | 'More',
@@ -7,10 +9,10 @@ interface ILeftButtonsProps{
     isActive?: boolean // ? işareti bir değişken tanımını zorun olmadığını opsiyonel olduğunu belirtir.
 }
 function LeftButtons(props: ILeftButtonsProps) {
+    const navigate = useNavigate();
     const {iconType,name,path, isActive} = props;
     const icons = ()=>{
-    switch(iconType){
-        // <i class="fa-solid fa-magnifying-glass"></i>
+    switch(iconType){       
             case 'Home': return <i className="fa-solid fa-house" style={{color: '#1da1f2'}}></i>
             case 'Search': return <i className="fa-solid fa-magnifying-glass" style={{color: '#1da1f2'}}></i>
             case 'Notifications': return <i className="fa-solid fa-bell" style={{color: '#1da1f2'}}></i>
@@ -18,22 +20,28 @@ function LeftButtons(props: ILeftButtonsProps) {
             case 'Message': return <i className="fa-regular fa-envelope" style={{color: '#1da1f2'}}></i>            
             case 'Lists': return <i className="fa-solid fa-list" style={{color: '#1da1f2'}}></i>            
             case 'Locations': return <i className="fa-regular fa-bookmark" style={{color: '#1da1f2'}}></i>            
-            case 'More': return <i className="fa-solid fa-ellipsis" style={{color: '#1da1f2'}}></i>            
-          
+            case 'More': return <i className="fa-solid fa-ellipsis" style={{color: '#1da1f2'}}></i>  
             default: return <></>;
         }
     }
+    const goToPath = ()=>{
+        navigate(path);
+    }
+    const getStyleName = ()=>{
+       return isActive ?  {fontWeight: 'bold'} :  {}
+    } 
   return (
-    <div className='row'>
+    <div onClick={goToPath} className='row x-btn-hover align-items-center'>
         <div className="col-2">
             {
                 icons()
             }
         </div>
-        <div className="col-10">
-            <label className='form-label' style={isActive ? {fontWeight: 'bold'} : {}}>{name}</label>
+        <div className="col-10 ">
+            <span style={getStyleName()}>{name}</span>
         </div>
     </div>
   )
 }
-export default LeftButtons
+// Eğer bir component e state değiştikçe durumu değişmiyor ise memo kullanın. (useStete, useSelector yok ise )
+export default React.memo(LeftButtons);
