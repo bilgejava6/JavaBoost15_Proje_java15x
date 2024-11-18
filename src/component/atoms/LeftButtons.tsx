c96af4d3f9878eae62094609be0fb62f139c43b7
bilgejava6 <bilgejava6@gmail.com>
2024-11-18 14:23:31 +0300

    butonlar eklendi.

--- src/component/atoms/LeftButtons.tsx
+++ src/component/atoms/LeftButtons.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 interface ILeftButtonsProps{
-    iconType: 'Home' | 'Search' | 'Notifications' | 'Message' | 'Profile',
+    iconType: 'Home' | 'Search' | 'Notifications' | 'Message'
+     | 'Profile' | 'Lists' | 'Locations' | 'More',
     name: string,
-    path: string // hangi url adresine (syfaya) yönlenecek
+    path: string, // hangi url adresine (syfaya) yönlenecek
+    isActive?: boolean // ? işareti bir değişken tanımını zorun olmadığını opsiyonel olduğunu belirtir.
 }
 function LeftButtons(props: ILeftButtonsProps) {
-    const {iconType,name,path} = props;
+    const {iconType,name,path, isActive} = props;
     const icons = ()=>{
     switch(iconType){
+        // <i class="fa-solid fa-magnifying-glass"></i>
             case 'Home': return <i className="fa-solid fa-house" style={{color: '#1da1f2'}}></i>
+            case 'Search': return <i className="fa-solid fa-magnifying-glass" style={{color: '#1da1f2'}}></i>
+            case 'Notifications': return <i className="fa-solid fa-bell" style={{color: '#1da1f2'}}></i>
+            case 'Profile': return <i className="fa-regular fa-user" style={{color: '#1da1f2'}}></i>
+            case 'Message': return <i className="fa-regular fa-envelope" style={{color: '#1da1f2'}}></i>            
+            case 'Lists': return <i className="fa-solid fa-list" style={{color: '#1da1f2'}}></i>            
+            case 'Locations': return <i className="fa-regular fa-bookmark" style={{color: '#1da1f2'}}></i>            
+            case 'More': return <i className="fa-solid fa-ellipsis" style={{color: '#1da1f2'}}></i>            
+          
             default: return <></>;
         }
     }
   return (
-    <>
+    <div className='row'>
         <div className="col-2">
             {
                 icons()
             }
         </div>
-        <div className="col-8">
-            <label className='form-label'>{name}</label>
+        <div className="col-10">
+            <label className='form-label' style={isActive ? {fontWeight: 'bold'} : {}}>{name}</label>
         </div>
-    </>
+    </div>
   )
 }
 export default LeftButtons
--- src/component/molecules/LeftMenuComponent.tsx
+++ src/component/molecules/LeftMenuComponent.tsx
@@ -3,21 +3,29 @@ import LeftButtons from '../atoms/LeftButtons'
 
 function LeftMenuComponent() {
   return (
-    <div className="row justify-content-center">
-        <div className="col-5">
+    <div className="row justify-content-end mt-5">
+        <div className="col-7">
             <div className="row mb-2">
-                <LeftButtons iconType='Home' name='Anasayfa' path='/home' />
+                <LeftButtons iconType='Home' name='Anasayfa' path='/home' isActive={true} />
             </div>
             <div className="row mb-2">
-                <LeftButtons iconType='Search' name='Keşfet' path='/home'  />
+                <LeftButtons iconType='Search' name='Keşfet' path='/search'  />
             </div>
             <div className="row mb-2">
-                <LeftButtons iconType='Notifications' name='Bildirimler' path='/home'  />
+                <LeftButtons iconType='Notifications' name='Bildirimler' path='/notification'  />
             </div>
             <div className="row mb-2">
-                <LeftButtons iconType='Profile' name='Profil' path='/home'  />
+                <LeftButtons iconType='Lists' name='Listeler' path='/lists'  />
             </div>
-                    
+            <div className="row mb-2">
+                <LeftButtons iconType='Locations' name='Yer İşaretleri' path='/locations'  />
+            </div>    
+            <div className="row mb-2">
+                <LeftButtons iconType='Profile' name='Profil' path='/profile'  />
+            </div>
+            <div className="row mb-2">
+                <LeftButtons iconType='More' name='Daha fazla' path='/more'  />
+            </div>            
         </div>
     </div>
   )
