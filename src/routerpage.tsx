e468344f4f8523b7e67c1f17c2338030b9985c79
bilgejava6 <bilgejava6@gmail.com>
2024-11-20 09:26:33 +0300

    Login & register page ler eklendi.

--- src/routerpage.tsx
+++ src/routerpage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     BrowserRouter, Route, Routes
 } from 'react-router-dom'
-import HomePage from './page/HomePage'
+import {HomePage} from './page/Home'
 function RouterPage() {
   return (
     <BrowserRouter>
